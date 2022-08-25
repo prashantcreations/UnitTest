4ad8148790ca3afee119a1ca60a1468cdb5243e3
prashantcreations <prashanttiwari276@gmail.com>
2022-08-25 12:31:26 +0530

    Junit : life cycle methods

--- UnitTest/src/main/java/com/example/testCase/JunitTestClass.java
+++ UnitTest/src/main/java/com/example/testCase/JunitTestClass.java
@@ -5,16 +5,62 @@ import static org.junit.jupiter.api.Assertions.assertNotEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 import static org.junit.jupiter.api.Assertions.assertNull;
 
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import com.example.juint.UnitTestApplication;
 
 public class JunitTestClass {
+	
+	/*life cycle methods
+	 * 
+	 * @BeforeEach : common setup call before each method 
+	 * 
+	 * @AfterEach : common clenup after each method
+	 * 
+	 * @BeforeAll : execute only once before all method
+	 * 
+	 * @AfterAll : execute only once after all method
+	 * 
+	 * 
+	 * 
+	 * 
+	 * */
+	
+	UnitTestApplication test;
+	
+	@BeforeAll
+	static void intiForAll() {
+		System.out.println("in before all");
+			
+	}
+	@AfterAll
+	static void cleanForAll(){
+		System.out.println("in after all");
+		
+	}
+	
+	@BeforeEach
+	void intitValueEach() {
+		
+		System.out.println("in beforeeach");
+		test = new UnitTestApplication();
+	}
+	
+	@AfterEach
+	void cleanForEach(){
+		System.out.println("in after each");
+		
+	}
+	
 
 	@Test
 	void checkAdd() {
 		
-		UnitTestApplication test = new UnitTestApplication();
+		
 		assertEquals(6,test.add(3, 3),"3+3 must be 6");
 		assertNotEquals(7,test.add(3, 3),"3+3 must be 6");
 	}
